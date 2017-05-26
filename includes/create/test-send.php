317b450772e43f8c9feaa96dd58b2f4514cacf38
samarth sikotara <samarthsikotara@gmail.com>
2017-05-26 08:47:55 +0530

    Test send add embedd function added

--- includes/create/test-send.php
+++ includes/create/test-send.php
@@ -160,7 +160,7 @@ for($i=0;$i<count($test_email_array);$i++)
 	$mail->AddAddress($test_email_array[$i], '');
 	$mail->AddEmbeddedImage("image002.png", "my-attach", "image002.png");
 	$mail->AddReplyTo($reply_to, $from_name);
-	$mail->Body = 'Your <b>HTML</b> with an embedded Image: <img src="cid:my-attach"> Here is an image!';
+	$mail->Body = 'Your <b>HTML</b> with an embedded Image: <img src=\"cid:my-attach\"> Here is an image!';
 	$mail->IsHTML(true);
 	if(file_exists('../../uploads/attachments/'.$campaign_id))
 	{
